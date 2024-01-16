b4648284c2d62575ce123e5f7090921adccc2beb
Hidy Kong <hidykong@gmail.com>
2024-01-15 22:44:42 -0500

    Create stats.cpp

---
+++ stats.cpp
@@ -0,0 +1,12 @@
+#include "stats.h"
+// Include header files
+
+// Implement the functions average, min, max, median
+
+double average(int x, int y, int z) {
+  double ret;
+
+  ret = (x + y + z) / 3.0;
+
+  return ret;
+}
