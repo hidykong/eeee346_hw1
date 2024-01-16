5e32320624cd77cc91f42211788a3cdf0bd8600c
Hidy Kong <hidykong@gmail.com>
2024-01-15 22:44:15 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "stats.h"
+#include <iostream>
+
+/*
+ *  Main function
+ */
+int main() {
+  int x;
+  int y;
+  int z;
+
+  // Set the precision for cout
+  std::cout.precision(2);
+
+  // Get three integers
+  std::cin >> x >> y >> z;
+
+  // Output Average (Show in fixed point)
+  std::cout << "Average: " << std::fixed << average(x, y, z) << std::endl;
+
+  // Finish
+  // Output Median, Min, Max
+
+  return 0;
+}
